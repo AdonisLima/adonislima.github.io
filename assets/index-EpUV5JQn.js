8acc6eb2b5dbe130f474f03e627612d8a5647e4f
adonis <adonis@kinvo.com.br>
2024-08-07 18:41:43 -0300

    Updates

--- assets/index-EpUV5JQn.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-4tQwAzF7.js";import"./react-vendor-5nDjdL3T.js";function i(){return t.jsx("div",{children:"404"})}export{i as default};
---
+++ assets/index-jtR-2jo0.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-cYqTYoSU.js";import"./react-vendor-5nDjdL3T.js";function i(){return t.jsx("div",{children:"404"})}export{i as default};
